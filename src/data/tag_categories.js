e7a2d417cff0862a0337fbd0e81fb856e0888c25
Jaskaran-COMP4139 <148276587+Jaskaran-COMP4139@users.noreply.github.com>
2024-05-18 16:01:14 +0100

    Update tag_categories.js

--- src/data/tag_categories.js
+++ src/data/tag_categories.js
@@ -1,9 +1,19 @@
 const userDefinedTagCategories = {
-	"type:system": {
-		"description": "paper describing a system"
+	"type": {
+		"description": "Appication or Survey"
 	},
-	"type:method": {
+	"method": {
 		"description": "methodology"
+	},
+	"submethod": {
+		"description": "submethodology"
+	},
+	"publicationtype": {
+		"description": "Publication type"
+	},
+	"evaluation": {
+		"description": "Evaluation methodology"
 	}
 }
+
 
